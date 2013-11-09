7257940e197c95605ecea753a6cd5ae473b27ca0
Sai Chaitanya <saichaitanya88@gmail.com>
2013-11-09 17:11:24 -0500

    removed seed data from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,30 +20,4 @@ user.role = "admin"
 user.save
 
 # sends confirmation email to administrator
-# UserMailer.registration_confirmation(user).deliver
-
-
-
-Player.create(:status => "active", :first_name=> "player", :last_name => "1", :bio => "player 1 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "2", :bio => "player 2 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "3", :bio => "player 3 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "4", :bio => "player 4 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "5", :bio => "player 5 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "6", :bio => "player 6 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "7", :bio => "player 7 bio")
-Player.create(:status => "active", :first_name=> "player", :last_name => "8", :bio => "player 8 bio")
-
-Division.create(:status => "active", :division_level => "A")
-
-
-(0..8).each do |i|
-
-user = User.new
-user.name = "User " + i.to_s
-user.email = "saichaitanya88+" + i.to_s + "@gmail.com"
-user.password = "1234512345"
-user.password_confirmation = "1234512345"
-user.role = "standard"
-user.save
-
-end
+UserMailer.registration_confirmation(user).deliver
